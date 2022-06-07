12abe940682c12adbfcc5d9d21ff349c0d8481c2
jvapp <jvapp@Josephs-MacBook-Pro.local>
2022-06-07 13:13:03 -0500

    Send email with smtp.

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -1,51 +1,31 @@
-from __future__ import print_function
 
-import base64
-from email.mime.text import MIMEText
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
 
-import google.auth
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
 
+# contacts = ['YourAddress@gmail.com', 'test@example.com']
 
-def gmail_create_draft():
-    """Create and insert a draft email.
-       Print the returned draft's message and id.
-       Returns: Draft object, including draft id and message meta data.
+msg = EmailMessage()
+msg['Subject'] = 'Check out Bronx as a puppy!'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = EMAIL_ADDRESS
 
-      Load pre-authorized user credentials from the environment.
-      TODO(developer) - See https://developers.google.com/identity
-      for guides on implementing OAuth2 for the application.
-    """
-    creds, _ = google.auth.default()
+msg.set_content('This is a plain text email')
 
-    try:
-        # create gmail api client
-        service = build('gmail', 'v1', credentials=creds)
-
-        message = MIMEText('This is automated draft mail')
-        message['To'] = 'gibbs.airflow@gmail.com'
-        message['From'] = 'gibbs.airflow@gmail.com'
-        message['Subject'] = 'Automated draft'
-        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-
-        create_message = {
-            'message': {
-                'raw': encoded_message
-            }
-        }
-        # pylint: disable=E1101
-        draft = service.users().drafts().create(userId="me",
-                                                body=create_message).execute()
-
-        print(F'Draft id: {draft["id"]}\nDraft message: {draft["message"]}')
-
-    except HttpError as error:
-        print(F'An error occurred: {error}')
-        draft = None
-
-    return draft
+msg.add_alternative("""\
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style="color:SlateGray;">This is an HTML Email!</h1>
+    </body>
+</html>
+""", subtype='html')
 
 
-if __name__ == '__main__':
-    gmail_create_draft()
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
